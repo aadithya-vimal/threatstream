8ec592aefe8973298685b3f8b4ce5cdf5128e690
aadithya-vimal <aadithyavimal.work@gmail.com>
2026-09-10 10:26:46 +0530

    style: Google Sans Flex typeface via self-hosted variable font

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource-variable/google-sans-flex/wght.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
